4fb6a3e8c5a536d4cff0739a4b7f402648894b9a
Shivam Raj <shivam.raj@kearney.com>
2025-09-07 13:41:08 +0530

    fiixed supbase export in voting route

--- src/app/api/vote/route.js
+++ src/app/api/vote/route.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function POST(request) {
   try {
